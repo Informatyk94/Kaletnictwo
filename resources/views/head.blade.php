79a2138fc4a0f121c911f81b21b00002824bcb81
Konrad <konradxd7@gmail.com>
2019-01-06 23:51:52 +0100

    head for desctop

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,13 +1,16 @@
 <div class="container">
-    <div class="row">
-        <div class="col-sm">
-            One of three columns
+    <div class="row head">
+        <div class="col-sm-12 col-xl-3">
+            <a href="tel:123456789">
+                <img class="head__phone" src="/image/icons/phone.png" alt="telefon" />
+                <div class="head__text">(+48) 123 456 789</div>
+            </a>
         </div>
-        <div class="col-sm">
-            One of three columns
+        <div class="col-sm-12 col-xl-6">
+            <h1 class="head__title">Kaletnictwo Skoczów</h1>
         </div>
-        <div class="col-sm">
-            One of three columns
+        <div class="col-sm-12 col-xl-3">
+            <img class="head__facebook" src="/image/icons/facebook.png" />
         </div>
     </div>
 </div>
